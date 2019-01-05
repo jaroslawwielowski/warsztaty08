4132269a6d87e6bbec9b3ca0f49c7990b3c84506
jaroslawwielowski <jaroslawwielowski@gmail.com>
2019-01-05 12:04:09 +0100

    clearProject

---
+++ src/main/java/com/example/warsztaty08/Warsztaty08Application.java
@@ -0,0 +1,14 @@
+package com.example.warsztaty08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Warsztaty08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Warsztaty08Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/warsztaty08/Warsztaty08ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.warsztaty08;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Warsztaty08ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
