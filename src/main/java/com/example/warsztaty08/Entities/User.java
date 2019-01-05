775a6a738924db15b99765232543d9468b6c2240
jaroslawwielowski <jaroslawwielowski@gmail.com>
2019-01-05 12:43:52 +0100

    user

--- src/main/java/com/example/warsztaty08/Entities/User.java
+++ src/main/java/com/example/warsztaty08/Entities/User.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class User {
@@ -11,4 +13,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
+
+    @NotBlank
+    @NotEmpty
+    private String login;
+
+    @NotBlank
+    @NotEmpty
+    private String password;
 }
