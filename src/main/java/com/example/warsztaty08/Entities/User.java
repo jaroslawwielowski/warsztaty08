8eafc2ce8edd9b7d1d688801468801993e5610af
jaroslawwielowski <jaroslawwielowski@gmail.com>
2019-01-05 12:36:42 +0100

    FirstEntity

---
+++ src/main/java/com/example/warsztaty08/Entities/User.java
@@ -0,0 +1,14 @@
+package com.example.warsztaty08.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+}
---
+++ src/main/java/com/example/warsztaty08/Enums/UserPrivileges.java
@@ -0,0 +1,5 @@
+package com.example.warsztaty08.Enums;
+
+public enum UserPrivileges {
+     Administrator, NormalUser;
+}
